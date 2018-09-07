05a9076d373bb44b107c35d69b780f116cdd80a9
csmcallister <scott.mcallister@gsa.gov>
2018-09-07 12:45:54 -0400

    ENH: add ability to update db with reviewed predictions

--- HSM/model/predict.py
+++ HSM/model/predict.py
@@ -126,35 +126,35 @@ class ClassifyNewData():
         self.normalize_corpus = normalize_corpus
         if model == 'Value':
             model_name = model+'test_roc_auc_best_estimator.pkl'
-            model_path = os.path.join("/Users",'scottmcallister','Desktop','HSM','model','best_estimators',model_name)
+            model_path = os.path.join("/Users",'scottmcallister','Desktop','GitHub','10x-qualitative-data','HSM','model','best_estimators',model_name)
             self.model = model_path
         else:
             self.model = None
+        id_path = os.path.join("/Users",'scottmcallister','Desktop','GitHub','10x-qualitative-data','HSM','pastResponseId.txt')
         try:
-            id_path = os.path.join("/Users",'scottmcallister','Desktop','HSM','pastResponseId.txt')
             with open(id_path,'r') as f:
                     lines = f.read().splitlines()
                     try:
                         self.penultimateResponseId = lines[1]
                     except IndexError:
-                        #if only 1 id, then that's the penultimate
-                        self.penultimateResponseId = lines[0]
+                        #if only 1 id, then there's no penultimate
+                        self.penultimateResponseId = None
         except FileNotFoundError:
-            self.penultimateResponseId = None
-            print("Can't predict when the db is empty. Run qualtrics.py first.")
+            print("Can't predict without data. Run qualtrics.py first.")
             sys.exit(0)
 
     def get_new_data(self):
         db = pd.read_csv(db_path)
+        if self.penultimateResponseId:
+            idx = db.index[db['ResponseID'] == self.penultimateResponseId].tolist()[0]
+            db = db.iloc[idx+1:]
         db['Normalized Value'] = db['Value'].apply(self.normalize_corpus)
         db = db.dropna(subset=['Value'])
-
         ordinal_questions = ["Experience Rating",
                              "Likely to Return",
                              "Likely to Recommend",
                              "Able to Accomplish"
                              ]
-
         for col in ordinal_questions:
             if col=='Likely to Return' or col=="Likely to Recommend":
                 db[col]= db[col].map({1:'Very unlikely',
@@ -175,46 +175,40 @@ class ClassifyNewData():
                                        3:'Fair',
                                        4:'Good',
                                        5:'Very good'})
-        #get index of penultimateResponseId
-        #idx = db[db['ResponseID']==self.penultimateResponseId].index[0]
-        #db.iloc[idx:,:]
         return db
 
     def predict(self):
         with open(self.model, 'rb') as f:
             pickled_model = pickle.load(f)
-
         new_data = self.get_new_data()
         preds = pickled_model.predict(new_data)
         try:
             pred_probs = pickled_model.predict_proba(new_data)
-            new_data['Spam Prediction'] = preds
+            new_data['Value Spam'] = preds
             new_data['Ham Prediction Probability'] = 0
             new_data['Spam Prediction Probability'] = 0
             new_data[['Ham Prediction Probability',
                       'Spam Prediction Probability']] = pd.DataFrame(pred_probs).values
             pred_prob_deltas = abs(new_data['Ham Prediction Probability'] - new_data['Spam Prediction Probability'])
-            new_data['Prediction Probabilities Delta'] = pred_prob_deltas
+            new_data['Value Prediction Probabilities Delta'] = pred_prob_deltas
             new_data = new_data.drop(labels=['Ham Prediction Probability',
                                              'Spam Prediction Probability'],
                                      axis=1)
         except AttributeError:
             #catch models that do not have the predict_proba() method
             dec_func = pickled_model.decision_function(new_data)
-            new_data['Spam Prediction'] = preds
-            new_data['Decision Boundary Distance'] = dec_func
-
-
+            new_data['Value Spam'] = preds
+            new_data['Value Decision Boundary Distance'] = abs(dec_func)
         results_dir = os.path.join(os.getcwd(),'results')
         if not os.path.exists(results_dir):
             os.makedirs(os.path.join(results_dir))
         results_path = os.path.join(results_dir,'ClassificationResults.xlsx')
         writer = pd.ExcelWriter(results_path)
-        new_data.to_excel(writer,'Classification Results')
+        new_data.to_excel(writer,'Classification Results',index=False)
         writer.save()
 
-
-db_path = os.path.join("/Users","scottmcallister","Desktop",'HSM','db','db.csv')
+db_path = os.path.join("/Users",'scottmcallister','Desktop','GitHub','10x-qualitative-data','HSM','db','db.csv')
 nd = ClassifyNewData(db_path, normalize_corpus,'Value')
 nd.get_new_data()
 nd.predict()
+print("Done making predictions. You can find the results in ClassificationResults.xlsx")
---
+++ HSM/update_db.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import os
+import sys
+
+class UpdateDb():
+
+    def __init__(self):
+        id_path = os.path.join("/Users",'scottmcallister','Desktop','GitHub','10x-qualitative-data','HSM','pastResponseId.txt')
+        try:
+            with open(id_path,'r') as f:
+                    lines = f.read().splitlines()
+                    try:
+                        self.penultimateResponseId = lines[1]
+                    except IndexError:
+                        #if only 1 id, then there's no penultimate
+                        self.penultimateResponseId = None
+        except FileNotFoundError:
+            print("Can't update the db without data! Run qualtrics.py first.")
+            sys.exit(0)
+
+    def update_db(self):
+        db_path = os.path.join('db','db.csv')
+        db_df = pd.read_csv(db_path)
+        if self.penultimateResponseId:
+            idx = db_df.index[db['ResponseID'] == self.penultimateResponseId].tolist()[0]
+            db_df = db_df.iloc[idx+1:]
+        db_id_pred_map = dict(zip(db_df['ResponseID'], db_df['Value Spam']))
+        results_path = os.path.join('model','results','ClassificationResults.xlsx')
+        results_df = pd.read_excel(results_path)
+        validated_id_pred_map = dict(zip(results_df['ResponseID'],
+                                         results_df['Value Spam']))
+        # For dictionaries db_id_pred_map and validated_id_pred_map, merged_id_pred_map
+        # will be a merged dictionary with values from validated_id_pred_map replacing
+        # those from db_id_pred_map
+        merged_id_pred_map = {**db_id_pred_map, **validated_id_pred_map}
+        num_merged_ids = len(merged_id_pred_map.keys())
+        num_db_ids = len(db_id_pred_map.keys())
+
+        if num_merged_ids > num_db_ids:
+            for k in merged_id_pred_map:
+                if k not in db_id_pred_map:
+                    print(f"This ResponseID isn't in the db:  {f}.")
+                    print("You shold probably look into this.")
+            db_df['Value Spam'] = db_df['ResponseID'].map(merged_id_pred_map)
+        elif num_merged_ids < num_db_ids:
+            print("The number of merged ids is less than the number of ids in the db.")
+            print(f"This will fill {num_db_ids-num_merged_ids} ids with nan!")
+            print("Exiting script")
+            sys.exit(0)
+        else:
+            db_df['Value Spam'] = db_df['ResponseID'].map(merged_id_pred_map)
+
+        db_df.to_csv(db_path,index=False)
+
+ud = UpdateDb()
+ud.update_db()
+print("Done updating the database with (hopefully reviewed) predictions.")
