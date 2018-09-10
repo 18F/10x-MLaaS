21224b048bdab181fd74bf29f8cabd3f684d94a6
csmcallister <scott.mcallister@gsa.gov>
2018-09-10 07:56:02 -0400

    CLN: removed pass statement

--- HSM/app.py
+++ HSM/app.py
@@ -2,16 +2,15 @@ import qualtrics
 import os
 from model import predict, train
 
+# Get the data from qualtrics
 qa = qualtrics.QualtricsApi()
 qa.download_responses()
 qa.update_db()
-
 model_path = os.path.join(os.getcwd(),
                           'model',
                           'best_estimators',
                           'Valuetest_roc_auc_best_estimator.pkl')
-if os.path.exists(model_path):
-    pass
+if os.spath.exists(model_path):
     print("A trained model already exists, so let's use it!")
     # TODO: check if we've hit a threshold for newly labeled data
     db_path = os.path.join(os.getcwd(),'db','db.csv')
