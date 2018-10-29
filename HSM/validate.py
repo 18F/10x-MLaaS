f614cc5ba11c608e5673f7efa908ac2590271369
csmcallister <scott.mcallister@gsa.gov>
2018-10-29 17:06:55 -0400

    ENH: add module for checking user validation

---
+++ HSM/validate.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+import sys
+
+class Validate():
+
+    def __init__(self, results_path):
+        self.results_path = results_path
+
+    
+    def get_validations(self):
+        '''
+        Returns a mapping of responseIds to user-validated spam/ham codes
+        '''
+        validated_df = pd.read_excel(self.results_path)
+        validated_id_pred_map = dict(zip(validated_df['ResponseID'],
+                                         validated_df['SPAM']))
+        return validated_id_pred_map
+
+        
+       
