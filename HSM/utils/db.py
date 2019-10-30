ac6b008b8c7c8d127673be5e15ed061463ae13f9
Amy Mok <amy.mok@gsa.gov>
2019-10-30 01:35:00 -0700

    Change Database Schema to a simpler schema (#93)
    * Add load_data script to load data to simple schema
    
    * Fix flake8 issues
    
    * Remove unnecessary print statements and add more informational print statements to show completion
    
    * Fix grammar

--- HSM/utils/db.py
+++ HSM/utils/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, JSON, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 from utils.config import SQLALCHEMY_URI
 
@@ -14,12 +14,30 @@ class DataAccessLayer:
         self.conn_string = SQLALCHEMY_URI
 
     def connect(self):
-        self.engine = create_engine(self.conn_string)
+        self.engine = create_engine(self.conn_string, echo=True)
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
 
 dal = DataAccessLayer()
+
+
+class Data(Base):
+    __tablename__ = 'data'
+    id = Column(Integer, primary_key=True, index=True)
+    filter_feature = Column(String(10000), nullable=True)
+    validation = Column(Integer)
+
+    support_data = relationship("SupportData", uselist=False, back_populates="data")
+
+
+class SupportData(Base):
+    __tablename__ = 'support_data'
+    id = Column(Integer, primary_key=True, index=True)
+    support_data = Column(JSON)
+
+    data_id = Column(Integer, ForeignKey('data.id'), nullable=False)
+    data = relationship("Data", back_populates="support_data")
 
 
 class Survey(Base):
