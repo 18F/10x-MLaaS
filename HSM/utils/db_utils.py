219435b0b8a8fe1e6d4aa65586273e89c5b462d3
Amy Mok <amy.mok@gsa.gov>
2019-11-05 22:27:18 -0800

    Convert code to use new db schema

--- HSM/utils/db_utils.py
+++ HSM/utils/db_utils.py
@@ -1,19 +1,45 @@
+import json
 import pandas as pd
 from utils.config import SQLALCHEMY_URI
 from sqlalchemy_utils import database_exists, create_database
 from utils.db import (
+    Data,
+    SupportData,
     Survey,
     Question,
     SurveyQuestion,
-    Respondent,
-    Response,
+    # Respondent,
+    # Response,
     Model,
     Version,
-    Prediction,
+    # Prediction,
     Validation,
-    VersionPrediction
+    # VersionPrediction
 )
-from sqlalchemy import desc, func,  create_engine
+from utils.config import ENTRY_ID, FILTER_FEATURE, VALIDATION
+from sqlalchemy import create_engine  # , desc, event, func
+from sqlalchemy import func
+# from sqlalchemy.engine import Engine
+# from sqlalchemy.sql.expression import func
+# import time
+# import logging
+
+# logging.basicConfig()
+# logger = logging.getLogger("HSM.sqltime")
+# logger.setLevel(logging.DEBUG)
+
+# @event.listens_for(Engine, "before_cursor_execute")
+# def before_cursor_execute(conn, cursor, statement,
+#                         parameters, context, executemany):
+#     conn.info.setdefault('query_start_time', []).append(time.time())
+#     logger.debug("Start Query: %s", statement)
+
+# @event.listens_for(Engine, "after_cursor_execute")
+# def after_cursor_execute(conn, cursor, statement,
+#                         parameters, context, executemany):
+#     total = time.time() - conn.info['query_start_time'].pop(-1)
+#     logger.debug("Query Complete!")
+#     logger.debug("Total Time: %f", total)
 
 
 def create_postgres_db():
@@ -23,144 +49,178 @@ def create_postgres_db():
         create_database(engine.url)
 
 
-def survey_exists(survey_name, survey_questions, session):
-    survey_names = []
-    for row in session.query(Survey.name).all():
-        survey_names.append(row.name)
+# def survey_exists(survey_name, survey_questions, session):
+#     survey_names = []
+#     for row in session.query(Survey.name).all():
+#         survey_names.append(row.name)
 
-    if survey_name in survey_names:
-        pass
-    else:
-        site_wide = Survey(name="Site-Wide Survey English")
-        for q in survey_questions:
-            sq = SurveyQuestion()
-            sq.question = Question(text=q)
-            site_wide.questions.append(sq)
-        session.add(site_wide)
-
-
-def model_exists(model_description, session):
-    model_description = model_description.replace(".pkl", "")
-    version_description = model_description.rsplit('_', 1)[-1]
-    model_description = model_description.replace("_" + version_description, "")
-    version = Version(description=version_description)
-    model_descriptions = []
-    # TODO: Why are we doing this instead of querying the database directly for the row.description match?)
-    for row in session.query(Model.description).all():
-        model_descriptions.append(row.description)
-    if model_description in model_descriptions:
-        pass
-    else:
-        model = Model(description=model_description)
-        model.versions.append(version)
-        session.add(model)
+#     if survey_name in survey_names:
+#         pass
+#     else:
+#         site_wide = Survey(name="Site-Wide Survey English")
+#         for q in survey_questions:
+#             sq = SurveyQuestion()
+#             sq.question = Question(text=q)
+#             site_wide.questions.append(sq)
+#         session.add(site_wide)
 
 
-def validation_exists(validation_set, session):
-    validations = []
-    for row in session.query(Validation.validation).all():
-        validations.append(row.validation)
-    diff = validation_set.difference(set(validations))
-    if len(diff) > 0:
-        validations_to_insert = []
-        for v in diff:
-            validation = Validation(validation=int(v))
-            validations_to_insert.append(validation)
-        session.add_all(validations_to_insert)
+# def model_exists(model_description, session):
+#     model_description = model_description.replace(".pkl", "")
+#     version_description = model_description.rsplit('_', 1)[-1]
+#     model_description = model_description.replace("_" + version_description, "")
+#     version = Version(description=version_description)
+#     model_descriptions = []
+#     # TODO: Why are we doing this instead of querying the database directly for the row.description match?)
+#     for row in session.query(Model.description).all():
+#         model_descriptions.append(row.description)
+#     if model_description in model_descriptions:
+#         pass
+#     else:
+#         model = Model(description=model_description)
+#         model.versions.append(version)
+#         session.add(model)
 
 
-def prediction_exists(prediction_set, session):
-    predictions = []
-    for row in session.query(Prediction.prediction).all():
-        predictions.append(row.prediction)
-    diff = prediction_set.difference(set(predictions))
-    if len(diff) > 0:
-        predictions_to_insert = []
-        for v in diff:
-            prediction = Prediction(prediction=int(v))
-            predictions_to_insert.append(prediction)
-        session.add_all(predictions_to_insert)
+# def validation_exists(validation_set, session):
+#     validations = []
+#     for row in session.query(Validation.validation).all():
+#         validations.append(row.validation)
+#     diff = validation_set.difference(set(validations))
+#     if len(diff) > 0:
+#         validations_to_insert = []
+#         for v in diff:
+#             validation = Validation(validation=int(v))
+#             validations_to_insert.append(validation)
+#         session.add_all(validations_to_insert)
 
 
-def insert_respondents(df, respondent_attributes, session):
-    # Get the number of rows here
-    for i in range(df.shape[0]):
-        if (i % 1000) == 0:
-            session.commit()
-        data = df.iloc[i][respondent_attributes]
-        respondent_data = {k: v for k, v in zip(respondent_attributes, data)}
-        respondent = Respondent(**respondent_data)
-        session.add(respondent)
+# def prediction_exists(prediction_set, session):
+#     predictions = []
+#     for row in session.query(Prediction.prediction).all():
+#         predictions.append(row.prediction)
+#     diff = prediction_set.difference(set(predictions))
+#     if len(diff) > 0:
+#         predictions_to_insert = []
+#         for v in diff:
+#             prediction = Prediction(prediction=int(v))
+#             predictions_to_insert.append(prediction)
+#         session.add_all(predictions_to_insert)
 
 
-def insert_responses(df, survey_questions, survey_name, model_description, session):
+# def insert_respondents(df, respondent_attributes, session):
+#     # Get the number of rows here
+#     for i in range(df.shape[0]):
+#         if (i % 1000) == 0:
+#             session.commit()
+#         data = df.iloc[i][respondent_attributes]
+#         respondent_data = {k: v for k, v in zip(respondent_attributes, data)}
+#         respondent = Respondent(**respondent_data)
+#         session.add(respondent)
 
-    def fetch_survey_id(survey_name, session):
-        survey_id = session.query(Survey.id).filter(Survey.name == survey_name).first().id
-        return survey_id
 
-    def fetch_question_id(question_name, session):
-        question_id = session.query(Question.id).filter(Question.text == question_name).first().id
-        return question_id
+# def insert_responses(df, survey_questions, survey_name, model_description, session):
 
-    def fetch_respondent_id(qualtrics_response_id, session):
-        respondent_id = session.query(Respondent.id).filter(Respondent.ResponseID == qualtrics_response_id).first().id
-        return respondent_id
+#     def fetch_survey_id(survey_name, session):
+#         survey_id = session.query(Survey.id).filter(Survey.name == survey_name).first().id
+#         return survey_id
 
-    def fetch_validation_id(validation, session):
-        validation_id = session.query(Validation.id).filter(Validation.validation == validation).first().id
-        return validation_id
+#     def fetch_question_id(question_name, session):
+#         question_id = session.query(Question.id).filter(Question.text == question_name).first().id
+#         return question_id
 
-    def fetch_model_version_ids(model_description, session):
-        model_description = model_description.replace(".pkl", "")
-        version_description = model_description.rsplit('_', 1)[-1]
-        model_description = model_description.replace("_" + version_description, "")
-        model_id = session.query(Model.id).filter(Model.description == model_description).first().id
-        version_id = session.query(Version.id).filter(Version.description == version_description).first().id
+#     def fetch_respondent_id(qualtrics_response_id, session):
+#         respondent_id = session.query(Respondent.id).filter(Respondent.ResponseID == qualtrics_response_id).first().id
+#         return respondent_id
 
-        return model_id, version_id
+#     def fetch_validation_id(validation, session):
+#         validation_id = session.query(Validation.id).filter(Validation.validation == validation).first().id
+#         return validation_id
 
-    def fetch_prediction_id(prediction, session):
-        prediction_id = session.query(Prediction.id).filter(Prediction.prediction == prediction).first().id
+#     def fetch_model_version_ids(model_description, session):
+#         model_description = model_description.replace(".pkl", "")
+#         version_description = model_description.rsplit('_', 1)[-1]
+#         model_description = model_description.replace("_" + version_description, "")
+#         model_id = session.query(Model.id).filter(Model.description == model_description).first().id
+#         version_id = session.query(Version.id).filter(Version.description == version_description).first().id
 
-        return prediction_id
+#         return model_id, version_id
 
-    survey_id = fetch_survey_id(survey_name, session)
-    model_id, version_id = fetch_model_version_ids(model_description, session)
+#     def fetch_prediction_id(prediction, session):
+#         prediction_id = session.query(Prediction.id).filter(Prediction.prediction == prediction).first().id
 
-    for i in range(df.shape[0]):
-        if (i % 250) == 0:
-            # Commit in batches.
-            session.commit()
-            session.flush()
-        data = df.iloc[i][survey_questions+['ResponseID', 'prediction', 'validated prediction']]
-        respondent_id = fetch_respondent_id(data['ResponseID'], session)
-        validation = int(data['validated prediction'])
-        validation_id = fetch_validation_id(validation, session)
-        pred = int(data['prediction'])
-        prediction_id = fetch_prediction_id(pred, session)
-        # prediction = session.query(Prediction).get(prediction_id)
-        val = session.query(Validation).get(validation_id)
-        for q in survey_questions:
-            question_id = fetch_question_id(q, session)
-            response = Response(survey_id=survey_id,
-                                question_id=question_id,
-                                respondent_id=respondent_id,
-                                text=data[q])
+#         return prediction_id
 
-            val.responses.append(response)
-            session.add(response)
-            # By flushing we can get the response ID
-            session.flush()
-            response_id = response.id
-            version_prediction = VersionPrediction()
-            version_prediction.model_id = model_id
-            version_prediction.version_id = version_id
-            version_prediction.prediction_id = prediction_id
-            version_prediction.response_id = response_id
-            # version_prediction.prediction = prediction
-            session.add(version_prediction)
-        session.commit()
+#     survey_id = fetch_survey_id(survey_name, session)
+#     model_id, version_id = fetch_model_version_ids(model_description, session)
+
+#     for i in range(df.shape[0]):
+#         if (i % 250) == 0:
+#             # Commit in batches.
+#             session.commit()
+#             session.flush()
+#         data = df.iloc[i][survey_questions+['ResponseID', 'prediction', 'validated prediction']]
+#         respondent_id = fetch_respondent_id(data['ResponseID'], session)
+#         validation = int(data['validated prediction'])
+#         validation_id = fetch_validation_id(validation, session)
+#         pred = int(data['prediction'])
+#         prediction_id = fetch_prediction_id(pred, session)
+#         # prediction = session.query(Prediction).get(prediction_id)
+#         val = session.query(Validation).get(validation_id)
+#         for q in survey_questions:
+#             question_id = fetch_question_id(q, session)
+#             response = Response(survey_id=survey_id,
+#                                 question_id=question_id,
+#                                 respondent_id=respondent_id,
+#                                 text=data[q])
+
+#             val.responses.append(response)
+#             session.add(response)
+#             # By flushing we can get the response ID
+#             session.flush()
+#             response_id = response.id
+#             version_prediction = VersionPrediction()
+#             version_prediction.model_id = model_id
+#             version_prediction.version_id = version_id
+#             version_prediction.prediction_id = prediction_id
+#             version_prediction.response_id = response_id
+#             # version_prediction.prediction = prediction
+#             session.add(version_prediction)
+#         session.commit()
+
+
+def insert_data(df, session):
+    '''
+    Insert data and supporting data into database
+    '''
+
+    data_columns = [FILTER_FEATURE, VALIDATION]
+
+    data = df[data_columns]
+    support_data = json.loads(df[df.columns.difference(data_columns)].to_json(orient='records'))
+
+    for i in range(len(data)):
+
+        data_row = data.iloc[i]
+        support_data_row = support_data[i]
+
+        data_obj = Data(filter_feature=str(data_row[FILTER_FEATURE]), validation=int(data_row[VALIDATION]))
+        session.add(data_obj)
+        session.flush()
+        support_data_obj = SupportData(support_data=support_data_row)
+        data_obj.support_data = support_data_obj
+        support_data_obj.data = data_obj
+        support_data_obj.data_id = support_data_obj.data.id
+        session.add(support_data_obj)
+
+    session.commit()
+
+
+def get_data(session, filter_feature='filter_feature', validation='validation'):
+
+    data_rows = [(row.filter_feature, row.validation) for row in session.query(Data).all()]
+    df = pd.DataFrame(data_rows, columns=[filter_feature, validation])
+    return df
 
 
 def fetch_last_RespondentID(session):
@@ -174,8 +234,9 @@ def fetch_last_RespondentID(session):
         last_response_id (str): the RespondentID of the last survey response
     '''
     try:
-        query_response = session.query(Respondent).order_by(desc('EndDate')).first()
-        last_response_id = query_response.ResponseID
+        last_response = session.query(Data).order_by(Data.id.desc()).first()
+        last_response_id = last_response.support_data.support_data[ENTRY_ID]
+
     except AttributeError:
         last_response_id = None
 
@@ -188,20 +249,20 @@ def count_table_rows(table, session):
     return rows
 
 
-def fetch_concatenated_comments(session):
+# def fetch_concatenated_comments(session):
 
-    def fetch_question_id(question_name, session):
-        question_id = session.query(Question.id).filter(Question.text == question_name).first().id
-        return question_id
+#     def fetch_question_id(question_name, session):
+#         question_id = session.query(Question.id).filter(Question.text == question_name).first().id
+#         return question_id
 
-    question_id = fetch_question_id("Comments_Concatenated", session)
-    rows = session.query(Response).filter_by(question_id=question_id).all()
-    responses = []
-    for row in rows:
-        if row.text:
-            responses.append(row.text)
+#     question_id = fetch_question_id("Comments_Concatenated", session)
+#     rows = session.query(Response).filter_by(question_id=question_id).all()
+#     responses = []
+#     for row in rows:
+#         if row.text:
+#             responses.append(row.text)
 
-    return responses
+#     return responses
 
 
 def prep_test_db(session):
@@ -209,20 +270,20 @@ def prep_test_db(session):
     Create test db and dummy data for testing
     '''
     survey_questions = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Comments_Concatenated']
-    respondent_attributes = ['Browser_Metadata_Q_1_TEXT', 'Browser_Metadata_Q_2_TEXT',
-                             'Browser_Metadata_Q_3_TEXT', 'Browser_Metadata_Q_4_TEXT',
-                             'Browser_Metadata_Q_5_TEXT', 'Browser_Metadata_Q_6_TEXT',
-                             'Browser_Metadata_Q_7_TEXT', 'CP_URL', 'Country', 'DeviceType',
-                             'EndDate', 'ExternalDataReference', 'Finished', 'History', 'IPAddress',
-                             'LocationAccuracy', 'LocationLatitude', 'LocationLongitude', 'PR_URL',
-                             'RecipientEmail', 'RecipientFirstName', 'RecipientLastName', 'Referer',
-                             'ResponseID', 'ResponseSet', 'SPAM', 'Site_Referrer', 'StartDate', 'State',
-                             'Status', 'TVPC', 'UPVC', 'UserAgent', 'Welcome_Text', 'pageType',
-                             'Comments_Concatenated']
-    pred_cols = ['prediction', 'validated prediction']
-    cols = survey_questions+respondent_attributes+pred_cols
-    d = {k: ['abc', '123'] for k in cols}
-    df = pd.DataFrame(d)
+    # respondent_attributes = ['Browser_Metadata_Q_1_TEXT', 'Browser_Metadata_Q_2_TEXT',
+    #                          'Browser_Metadata_Q_3_TEXT', 'Browser_Metadata_Q_4_TEXT',
+    #                          'Browser_Metadata_Q_5_TEXT', 'Browser_Metadata_Q_6_TEXT',
+    #                          'Browser_Metadata_Q_7_TEXT', 'CP_URL', 'Country', 'DeviceType',
+    #                          'EndDate', 'ExternalDataReference', 'Finished', 'History', 'IPAddress',
+    #                          'LocationAccuracy', 'LocationLatitude', 'LocationLongitude', 'PR_URL',
+    #                          'RecipientEmail', 'RecipientFirstName', 'RecipientLastName', 'Referer',
+    #                          'ResponseID', 'ResponseSet', 'SPAM', 'Site_Referrer', 'StartDate', 'State',
+    #                          'Status', 'TVPC', 'UPVC', 'UserAgent', 'Welcome_Text', 'pageType',
+    #                          'Comments_Concatenated']
+    # pred_cols = ['prediction', 'validated prediction']
+    # cols = survey_questions+respondent_attributes+pred_cols
+    # d = {k: ['abc', '123'] for k in cols}
+    # df = pd.DataFrame(d)
     # set up survey and questions
     survey_name = "Test Survey"
     survey = Survey(name=survey_name)
@@ -232,7 +293,7 @@ def prep_test_db(session):
         survey.questions.append(sq)
     session.add(survey)
     # set up model and a version
-    model_description = 'Test Model'
+    # model_description = 'Test Model'
     model = Model(description='Test Model')
     version = Version(description='Test Version')
     model.versions.append(version)
@@ -250,7 +311,7 @@ def prep_test_db(session):
         predictions_to_insert.append(prediction)
     session.add_all(predictions_to_insert)
     # set up respondents
-    insert_respondents(df, respondent_attributes, session)
+    # insert_respondents(df, respondent_attributes, session)
     # set up respones
-    insert_responses(df, survey_questions, survey_name, model_description, session)
+    # insert_responses(df, survey_questions, survey_name, model_description, session)
     session.commit()
